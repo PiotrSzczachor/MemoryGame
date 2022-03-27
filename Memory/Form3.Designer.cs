104ec042ee799ce5462116764cc6de2281151833
PiotrSzczachor <piotr.szczachor1@wp.pl>
2022-03-27 16:40:38 +0200

    Added cards&backgroun graphics, CardsNames&FilesNames lists, CardsOpenTime changing while playing

--- Memory/Form3.Designer.cs
+++ Memory/Form3.Designer.cs
@@ -29,25 +29,96 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form3));
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.setCardsOpenTimeTo10sToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.secToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.secToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.settingsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1265, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.setCardsOpenTimeTo10sToolStripMenuItem});
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // setCardsOpenTimeTo10sToolStripMenuItem
+            // 
+            this.setCardsOpenTimeTo10sToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sToolStripMenuItem,
+            this.secToolStripMenuItem,
+            this.secToolStripMenuItem1});
+            this.setCardsOpenTimeTo10sToolStripMenuItem.Name = "setCardsOpenTimeTo10sToolStripMenuItem";
+            this.setCardsOpenTimeTo10sToolStripMenuItem.Size = new System.Drawing.Size(184, 22);
+            this.setCardsOpenTimeTo10sToolStripMenuItem.Text = "Set Cards Time Open";
+            // 
+            // sToolStripMenuItem
+            // 
+            this.sToolStripMenuItem.Name = "sToolStripMenuItem";
+            this.sToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.sToolStripMenuItem.Text = "10 sec.";
+            this.sToolStripMenuItem.Click += new System.EventHandler(this.sToolStripMenuItem_Click);
+            // 
+            // secToolStripMenuItem
+            // 
+            this.secToolStripMenuItem.Name = "secToolStripMenuItem";
+            this.secToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.secToolStripMenuItem.Text = "6 sec.";
+            this.secToolStripMenuItem.Click += new System.EventHandler(this.secToolStripMenuItem_Click);
+            // 
+            // secToolStripMenuItem1
+            // 
+            this.secToolStripMenuItem1.Name = "secToolStripMenuItem1";
+            this.secToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+            this.secToolStripMenuItem1.Text = "2 sec.";
+            this.secToolStripMenuItem1.Click += new System.EventHandler(this.secToolStripMenuItem1_Click);
             // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(1265, 666);
+            this.Controls.Add(this.menuStrip1);
+            this.DoubleBuffered = true;
+            this.MainMenuStrip = this.menuStrip1;
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Form3";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form3";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form3_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem setCardsOpenTimeTo10sToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem secToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem secToolStripMenuItem1;
     }
 }
--- Memory/Form3.cs
+++ Memory/Form3.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +15,9 @@ namespace Memory
     {
 
         List<PictureBox> Cardslist = new List<PictureBox>();
+        List<string> FilesNames = null;
+        List<string> CardsNames = null;
+
         public Form3()
         {
             InitializeComponent();
@@ -21,7 +25,7 @@ namespace Memory
             int iterations = Settings.getInstance().getNumberOfCards();
             int x = 10;
             int y = 10;
-            for (int i=1; i<=iterations; i++)
+            /*for (int i=1; i<=iterations; i++)
             {
                 if (iterations == 48)
                 {
@@ -83,10 +87,54 @@ namespace Memory
 
                 }
             }
-            DrawCards();
+            drawCards();*/
+            getCardsAndFilesNames();
         }
 
-        private void DrawCards()
+        
+        private void getCardsAndFilesNames()
+        {
+            DirectoryInfo dir1 = new DirectoryInfo(@"C:\Users\Piotr\source\repos\Memory\Memory\Cards\");
+            FileInfo[] files = dir1.GetFiles("*.png", SearchOption.AllDirectories);
+
+            //Saving files names with .png extension into FilesNames list
+
+            if (FilesNames == null)
+            {
+                FilesNames = new List<string>();
+            }
+            else
+            {
+                FilesNames.Clear();
+            }
+
+            foreach (FileInfo f in files)
+            {
+                FilesNames.Add(f.Name);
+            }
+
+           //saving files names without .png extenxion but with _ at the end into CardsNames list
+
+            if (CardsNames == null)
+            {
+                CardsNames = new List<string>();
+            }
+            else
+            {
+                CardsNames.Clear();
+            }
+
+            foreach (string s in FilesNames)
+            {
+                string[] parts = s.Split('.');
+                CardsNames.Add(parts[0]+'_');
+            }
+            
+
+        }
+
+        //Adding PictureBoxes to Controls
+            private void drawCards()
         {
             foreach (PictureBox picture in Cardslist)
             {
@@ -102,6 +150,22 @@ namespace Memory
         private void pictureBox1_Click(object sender, EventArgs e)
         {
 
+        }
+
+        //Changing CardsOpenTime while playing
+        private void sToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Settings.getInstance().setCardsOpenTime(10);
+        }
+
+        private void secToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Settings.getInstance().setCardsOpenTime(6);
+        }
+
+        private void secToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            Settings.getInstance().setCardsOpenTime(2);
         }
     }
 }
